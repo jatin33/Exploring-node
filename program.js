d6146aa0c4149dcee6b080ee4c4c1e6d7d9dcee2
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-04-18 17:50:26 +0530

    Sample client Http get request

--- program.js
+++ program.js
@@ -5,20 +5,32 @@
 // console.log(sum);
 
 // my-first-async-io
+// ========================
 // const fs = require('fs');
 // fs.readFile(process.argv[2], 'utf8', function (err, data) {
 //     console.log(data.split('\n').length - 1);
 // });
 
-const filteredls = require('./my-filtered-ls');
-const directory = process.argv[2];
-const extName = process.argv[3];
+// Making filtered-ls utility modular
+// =====================================
+// const filteredls = require('./my-filtered-ls');
+// const directory = process.argv[2];
+// const extName = process.argv[3];
+// filteredls(directory, extName, function (err, list) {
+//     if (err) {
+//         return 'It was an error' + console.err(err);
+//     }
+//     list.forEach(element => {
+//         console.log(element);
+//     });
+// });
 
-filteredls(directory, extName, function (err, list) {
-    if (err) {
-        return 'It was an error' + console.err(err);
-    }
-    list.forEach(element => {
-        console.log(element);
+const http = require('http');
+// console.log(process.argv[2]);
+http.get(process.argv[2], (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (d) => {
+        console.log(d);
     });
-});
+    res.on('error', console.error);
+}).on('error', console.error);
